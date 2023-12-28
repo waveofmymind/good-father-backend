ba2f0c9d36652d2e119309fc86e01c9845ea0ec3
waveofmymind <sjun0913@gmail.com>
2023-12-29 03:02:27 +0900

    feat(root): 프로젝트 초기 설정을 구성한다.

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "good-father-backend"
---
+++ src/test/kotlin/com/goodfather/GoodFatherBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.goodfather
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoodFatherBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
