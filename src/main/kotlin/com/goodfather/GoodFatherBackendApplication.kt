9709495051a7000d7f67b37f7af482e43b7cf3a1
waveofmymind <sjun0913@gmail.com>
2023-12-29 03:03:45 +0900

    style(root): 부트 클래스 네이밍을 변경한다.

--- src/main/kotlin/com/goodfather/GoodFatherBackendApplication.kt
+++ src/main/kotlin/com/goodfather/GoodFatherBackendApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class GoodFatherBackendApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<GoodFatherBackendApplication>(*args)
+    runApplication<Application>(*args)
 }
